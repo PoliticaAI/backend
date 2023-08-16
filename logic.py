4ddee9a32170d83ee3c017b446a4330289785b06
Ayush Garg <ayushgargemail@gmail.com>
2023-08-15 22:17:30 -0700

    barebones api

---
+++ logic.py
@@ -0,0 +1,12 @@
+def get_ai_rating(url):
+  return -1
+
+def get_political_rating(url):
+  return -1
+
+def get_factuality_rating(url):
+  return -1
+
+def get_summary(url):
+  return "bla bla bla"
+

385e0e5c4473c0bc19ab5a0de7de09dc052e515d
Ayush Garg <ayushgargemail@gmail.com>
2023-10-07 23:46:37 -0700

    added CORS

--- api/index.py
+++ api/index.py
@@ -5,13 +5,17 @@ import uuid
 import services.llm as llm
 import services.duckduckgo as ddg
 
+from flask_cors import CORS
+
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 process_status = {}
 
-@app.route('/start_analysis', methods=['POST'])
+
+@app.route("/start_analysis", methods=["POST"])
 def start_analysis():
-    url = request.json['url']
+    url = request.json["url"]
 
     def analysis(url):
         final_response = None
@@ -19,29 +23,39 @@ def start_analysis():
         try:
             while True:
                 try:
-                    process_status[process_id] = {'status': 'running llm', 'progress': 1}
+                    process_status[process_id] = {
+                        "status": "running llm",
+                        "progress": 1,
+                    }
                     gpt_response = llm.GPTAnalyzer.analyze_article(url)
                     break
                 except:
-                    process_status[process_id] = {'status': 'retrying llm', 'progress': 1}
+                    process_status[process_id] = {
+                        "status": "retrying llm",
+                        "progress": 1,
+                    }
                     pass
 
-            process_status[process_id] = {'status': 'finished llm', 'progress': 2}
+            process_status[process_id] = {"status": "finished llm", "progress": 2}
 
-            ddg_response = ddg.SearchEngine.get_links(gpt_response['title'])
+            ddg_response = ddg.SearchEngine.get_links(gpt_response["title"])
             ddg_response = ddg.SearchEngine.filter_links(ddg_response, url)
 
-            process_status[process_id] = {'status': 'finished ddg', 'progress': 3}
+            process_status[process_id] = {"status": "finished ddg", "progress": 3}
 
             final_response = {
-                'gpt_response': gpt_response,
-                'ddg_response': ddg_response
+                "gpt_response": gpt_response,
+                "ddg_response": ddg_response,
             }
 
-            process_status[process_id] = {'status': 'completed', 'result': final_response, 'progress': 4}
+            process_status[process_id] = {
+                "status": "completed",
+                "result": final_response,
+                "progress": 4,
+            }
         except Exception as e:
             print(e)
-            process_status[process_id] = {'status': 'failed', 'progress': 0}
+            process_status[process_id] = {"status": "failed", "progress": 0}
 
         return final_response
 
@@ -50,31 +64,30 @@ def start_analysis():
     task_thread = threading.Thread(target=analysis, args=(url,))
     task_thread.start()
 
-    process_status[process_id] = {'status': 'started', 'progress': 0}
+    process_status[process_id] = {"status": "started", "progress": 0}
 
     # Return the process ID and status URL
-    response_data = {
-        'process_id': process_id,
-        'status_url': f'/status/{process_id}'
-    }
+    response_data = {"process_id": process_id, "status_url": f"/status/{process_id}"}
 
     return jsonify(response_data), 202
 
-@app.route('/status/<process_id>', methods=['GET'])
+
+@app.route("/status/<process_id>", methods=["GET"])
 def check_status(process_id):
     if process_id in process_status:
-        status = process_status[process_id]['status']
-        progress = process_status[process_id]['progress']
+        status = process_status[process_id]["status"]
+        progress = process_status[process_id]["progress"]
 
-        if status == 'completed':
-            result = process_status[process_id]['result']
-            return jsonify({'status': status, 'progress': progress, 'result': result})
-        elif status == 'failed':
-            return jsonify({'status': status, 'progress': progress}), 500
+        if status == "completed":
+            result = process_status[process_id]["result"]
+            return jsonify({"status": status, "progress": progress, "result": result})
+        elif status == "failed":
+            return jsonify({"status": status, "progress": progress}), 500
         else:
-            return jsonify({'status': status, 'progress': progress})
+            return jsonify({"status": status, "progress": progress})
     else:
-        return jsonify({'error': 'Process ID not found'}), 404
+        return jsonify({"error": "Process ID not found"}), 404
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
